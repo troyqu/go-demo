3d49aa869edf7bf25565ad8a8d2beb2393b23d6e
qujianfei <qujianfei@hotmail.com>
2021-11-16 16:45:33 +0800

    go-gin demo工程初始化

---
+++ go-gin/go-gin.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"fmt"
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
+
+func main() {
+	router := gin.Default()
+
+	router.LoadHTMLGlob("./*.html")
+
+	router.GET("/go/login", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "index.html", nil)
+	})
+
+	router.GET("/go/troy", func(c *gin.Context) {
+		fmt.Println("欢迎点击查看troy，马上跳转到troy-info")
+		c.Redirect(301, "http://127.0.0.1:8888/go/troy-info")
+	})
+
+	type msg struct {
+		Name    string `json:"name"`
+		Message string `json:"message"`
+		Age     int    `json:"age"`
+	}
+
+	router.GET("/go/troy-info", func(c *gin.Context) {
+		data := msg{
+			Name:    "troy",
+			Message: "平平无奇小书童",
+			Age:     18,
+		}
+		c.JSON(200, data)
+	})
+
+	router.Run(":8888")
+}
---
+++ go-gin/go.mod
@@ -0,0 +1,5 @@
+module gogin
+
+go 1.15
+
+require github.com/gin-gonic/gin v1.7.4
